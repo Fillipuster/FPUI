e8a341e980a40483dc6a474e814b17184942a7e6
Jonas Priestyard <contact@fillipuster.com>
2019-07-04 18:52:29 +0200

    V2 frame should be done.

--- lua/fpui/FPFrame.lua
+++ lua/fpui/FPFrame.lua
@@ -1,68 +1,65 @@
-local PANEL = {};
+local PANEL = {}
+
+DEFINE_BASECLASS("DFrame")
+
+AccessorFunc(PANEL, "m_allowKeyClose", "AllowKeyClose", FORCE_BOOL)
+AccessorFunc(PANEL, "m_closeKey", "CloseKey", FORCE_NUMBER)
 
 function PANEL:Init()
-	self:SetSize(300, 200);
+	BaseClass.Init(self)
 
-	self.rubrik = self.rubrik or "";
-	self.closeKey = KEY_E;
-	self.allowKeyClose = true;
+	// Disable all the default buttons and labels
+	for _,child in pairs(self:GetChildren()) do
+		child:SetVisible(false)
+	end
 
-	self:MakeCloseButton();
+	self:SetCloseKey(KEY_E)
+	self:SetAllowKeyClose(true)
+
+	self:MakeCloseButton()
 end
 
-function PANEL:SetRubrik(title)
-	self.rubrik = tostring(title);
-end
-
-function PANEL:Resize(x, y)
-	self:SetSize(x, y);
-	self:MakeCloseButton();
+function PANEL:PerformLayout()
+	BaseClass.PerformLayout(self)
+	self.closeButton:SetPos(self:GetWide() - 20, 5)
 end
 
 function PANEL:MakeCloseButton()
 	if (self.closeButton) then
-		self.closeButton:Remove();
+		self.closeButton:Remove()
 	end
 
-	local cb = vgui.Create("DButton", self);
-	cb:SetSize(15, 15);
-	cb:SetPos(self:GetWide() - 17.5, 2.5);
-	cb:SetText("");
+	local cb = vgui.Create("DButton", self)
+	cb:SetSize(15, 15)
+	cb:SetPos(self:GetWide() - 20, 5)
+	cb:SetText("")
 	cb.Paint = function(w, h)
-		draw.SimpleText("x", FPUI.Font(20, false), 7, 6, cb.hover and Color(255, 255, 255) or Color(200, 200, 200), 1, 1);
+		draw.SimpleText("x", "Open-Sans30", 7.5, 3.5, cb.hover and Color(255, 255, 255) or Color(200, 200, 200), 1, 1)
 	end
 	cb.DoClick = function()
-		self:Remove();
+		self:Remove()
 	end
 	cb.OnCursorEntered = function()
-		cb.hover = true;
+		cb.hover = true
 	end
 	cb.OnCursorExited = function()
-		cb.hover = false;
+		cb.hover = false
 	end
 
-	self.closeButton = cb;
+	self.closeButton = cb
 end
 
 function PANEL:Paint(w, h)
-	draw.RoundedBoxEx(2, 0, 0, w, 20, FPUI.config.baseDark, true, true, false, false);
-	draw.RoundedBoxEx(2, 0, 20, w, h - 20, FPUI.config.baseMedium, false, false, true, true);
+	draw.RoundedBoxEx(6, 0, 0, w, 24, FPUI.config.baseDark, true, true, false, false)
+	draw.RoundedBoxEx(6, 0, 24, w, h - 24, FPUI.config.baseMedium, false, false, true, true)
 
-	draw.SimpleText(self.rubrik, FPUI.Font(15), 5, 2.5, Color(255, 255, 255));
-end
-
-function PANEL:AllowKeyClose(allow)
-	self.allowKeyClose = allow;
-end
-
-function PANEL:SetCloseKey(key)
-	self.closeKey = key;
+	draw.SimpleText(self:GetTitle(self), "Poppins24", 5, 2.5, Color(255, 255, 255));
 end
 
 function PANEL:OnKeyCodePressed(key)
-	if (self.allowKeyClose && key == self.closeKey) then
-		self:Remove();
+	if (self:GetAllowKeyClose() and key == self:GetCloseKey()) then
+		self:Close()
 	end
 end
 
-vgui.Register("FPFrame", PANEL, "EditablePanel");
+vgui.Register("FPFrame", PANEL, "DFrame")
