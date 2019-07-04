514eddfdfb0bc1c61d6842d2a3908d39275f4a00
Jonas Priestyard <contact@fillipuster.com>
2019-07-04 18:52:33 +0200

    New config.

--- lua/fpui_config.lua
+++ lua/fpui_config.lua
@@ -1,8 +1,19 @@
-FPUI = FPUI or {};
-FPUI.config = FPUI.config or {};
+FPUI = FPUI or {}
+FPUI.config = FPUI.config or {}
 
+--[[----------------------------------------------------------
+Fillipuster's User Interface Configuration
+------------------------------------------------------------]]
+
+// The light shade of the base color of the UI:
 FPUI.config.baseLight = Color(54, 57, 62)
 
+// The standard shade of the base color of the UI:
 FPUI.config.baseMedium = Color(47, 49, 54)
 
+// The dark shade of the base color of the UI:
 FPUI.config.baseDark = Color(32, 34, 37)
+
+--[[----------------------------------------------------------
+End of Configuration
+------------------------------------------------------------]]
