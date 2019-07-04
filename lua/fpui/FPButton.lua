d8f85de8c449677458d89c2d23801f413a05c926
Jonas Priestyard <contact@fillipuster.com>
2019-07-04 22:02:34 +0200

    Much more efficient color system.

--- lua/fpui/FPButton.lua
+++ lua/fpui/FPButton.lua
@@ -4,7 +4,6 @@ local PANEL = {}
 
 DEFINE_BASECLASS("DButton")
 
-AccessorFunc(PANEL, "m_paintColor", "Color")
 AccessorFunc(PANEL, "m_disabledColor", "DisabledColor")
 
 function PANEL:Init()
@@ -13,34 +12,49 @@ function PANEL:Init()
 	self:SetFont("Open-Sans24")
 	self:SetText("Button")
 	self:SetTextColor(Color(255, 255, 255))
+
+	self:SetColor(Color(114, 137, 218))
+	self:SetDisabledColor(Color(91, 69, 69))
+end
+
+function PANEL:SetColor(color)
+	self.color = color
+	self.paintColor = color
+	self.downColor = Color(color.r - 25, color.g - 25, color.b - 25)
+	self.hoverColor = Color(color.r + 25, color.g + 25, color.b + 25)
+end
+
+function PANEL:GetColor()
+	return self.color
 end
 
 function PANEL:SetDisabled(disable)
 	BaseClass.SetDisabled(self, disable)
 
 	self:SetTextColor(disable and Color(200, 200, 200) or Color(255, 255, 255))
+	self.paintColor = disable and self:GetDisabledColor() or self:GetColor()
 end
 
 function PANEL:Paint(width, height)
-	local color = self:GetColor() or Color(114, 137, 218)
+	draw.RoundedBox(8, 0, 0, width, height, self:IsDown() and self.downColor or self.paintColor)
+end
 
-	local highlightColor = Color(color.r + 25, color.g + 25, color.b + 25)
-	local downColor = Color(color.r - 25, color.g - 25, color.b - 25)
-	
-	if (self.Hovered) then color = highlightColor end
-	if (self:IsDown()) then color = downColor end
-	if (self:GetDisabled()) then color = self:GetDisabledColor() or Color(91, 69, 69) end
+function PANEL:OnCursorEntered()
+	BaseClass.OnCursorEntered(self)
 
 	if (not self:GetDisabled()) then
-		local _, _, brightness = ColorToHSV(color)
-		if (brightness > 0.9) then
-			self:SetTextColor(FPUI.config.baseDark)
-		else
-			self:SetTextColor(Color(255, 255, 255))
-		end
+		self.paintColor = self.hoverColor
 	end
-
-	draw.RoundedBox(8, 0, 0, width, height, color)
 end
+
+function PANEL:OnCursorExited()
+	BaseClass.OnCursorExited(self)
+
+	if (not self:GetDisabled()) then
+		self.paintColor = self:GetColor()
+	end
+end
+
+
 
 vgui.Register("FPButton", PANEL, "DButton")
